ed6439b06c173495ca2df843feaa32791f7029b7
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-24 05:55:28 +0100

    first commit to upload all files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <p className="text-xl font-medium text-sky-700 text-center pt-4">
+      Started work
+    </p>
+  );
+}
