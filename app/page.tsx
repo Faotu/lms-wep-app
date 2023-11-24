aae5173a51a55419de689050a9f4251d79060822
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-24 07:00:48 +0100

    button update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-xl font-medium text-sky-700 text-center pt-4">
-      Started work
-    </p>
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-xl font-medium text-sky-700 text-center pt-4">
+        Started work
+      </p>
+      <Button variant="destructive">Click</Button>
+    </div>
   );
 }
