b071ea5c576391b5a518f16b33bb900c1d62f908
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-26 06:16:37 +0100

    signin routes and auth

--- app/(mydashboard)/(routes)/page.tsx
+++ app/(mydashboard)/(routes)/page.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <p>The protected sign in page</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
