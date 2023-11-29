0579ec2615307ef189f52cd63a3774a2c65e2936
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-29 16:54:00 +0100

    form input

--- app/(mydashboard)/instructor/create/page.tsx
+++ app/(mydashboard)/instructor/create/page.tsx
@@ -1,7 +1,37 @@
 "use client";
 import React from "react";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+});
 
 const CreatePage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return <div>Create page </div>;
 };
 
