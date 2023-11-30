2d1d3016a1c1a8a9a712240068fa4920c3250775
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-30 03:33:10 +0100

    Toast providers

--- app/(mydashboard)/instructor/create/page.tsx
+++ app/(mydashboard)/instructor/create/page.tsx
@@ -5,7 +5,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import {
   Form,
   FormControl,
@@ -23,6 +23,7 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,8 +32,13 @@ const CreatePage = () => {
   });
 
   const { isSubmitting, isValid } = form.formState;
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("api/course", values);
+      router.push(`/instructor/courses/${response.data.id}`);
+    } catch {
+      console.log("Something went wrong!");
+    }
   };
   return (
     <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
---
+++ components/providers/ToastProviders.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+const ToastProviders = () => {
+  return <Toaster />;
+};
+
+export default ToastProviders;
