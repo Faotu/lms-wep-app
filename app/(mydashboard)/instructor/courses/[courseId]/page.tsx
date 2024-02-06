0ccfe95b04eaf0a455945429ca18fe4dbbad38bf
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-02-06 19:19:34 +0100

    progessing component

--- app/(mydashboard)/instructor/courses/[courseId]/page.tsx
+++ app/(mydashboard)/instructor/courses/[courseId]/page.tsx
@@ -29,7 +29,28 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
 
-  return <div>Course Id: {params.courseId}</div>;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course set up</h1>
+
+          <span className="text-sm text-slate-700 ">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2 ">
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
