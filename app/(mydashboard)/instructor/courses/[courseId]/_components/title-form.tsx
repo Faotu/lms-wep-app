77b57425556cb2168b479afd6376ee8527f8d2c8
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-02-08 18:03:41 +0100

    edit form component

--- app/(mydashboard)/instructor/courses/[courseId]/_components/title-form.tsx
+++ app/(mydashboard)/instructor/courses/[courseId]/_components/title-form.tsx
@@ -1,5 +1,65 @@
 "use client";
 
-export const TitleForm = () => {
-  return <div>Title Form</div>;
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Form, FormControl, FormItem, FormMessage } from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(5, { message: "Title is required" }),
+});
+export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4 ">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <Pencil className="h-4 w-4 mr-2 " />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          ></form>
+        </Form>
+      )}
+    </div>
+  );
 };
